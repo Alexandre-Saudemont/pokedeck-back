5738c33c6c8a07f278abbf16d1d40ccf4dc344c0
Alexandre S <xetoxin@gmail.com>
2024-10-21 16:08:39 +0200

    .env ecemple update + DB pour production

--- app/config/database.js
+++ app/config/database.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize('pokedeck', 'postgres', 'poule', {
-	host: 'localhost',
+// CONFIG LOCAL
+// const sequelize = new Sequelize('pokedeck', 'postgres', 'poule', {
+// 	host: 'localhost',
+// 	dialect: 'postgres',
+// 	define: {timestamps: false},
+// });
+
+// CONFIG PRODUCTION
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+	host: process.env.DB_HOST,
 	dialect: 'postgres',
 	define: {timestamps: false},
 });
