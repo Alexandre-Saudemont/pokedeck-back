789b3addfdac29e73fcf84631be7d98f97dd91ae
Alexandre-Saudemont <alexandre.saudemont.pro@gmail.com>
2025-03-17 14:28:07 +0100

    config online

--- app/config/database.js
+++ app/config/database.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 const {Sequelize} = require('sequelize');
 
 // CONFIG LOCAL
-const sequelize = new Sequelize('pokedeck', 'alex', 'poule', {
-	host: 'localhost',
-	dialect: 'postgres',
-	define: {timestamps: false},
-});
-
-// CONFIG PRODUCTION
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-// 	host: process.env.DB_HOST,
+// const sequelize = new Sequelize('pokedeck', 'alex', 'poule', {
+// 	host: 'localhost',
 // 	dialect: 'postgres',
 // 	define: {timestamps: false},
 // });
+
+// CONFIG PRODUCTION
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+	host: process.env.DB_HOST,
+	dialect: 'postgres',
+	define: {timestamps: false},
+});
 
 sequelize
 	.authenticate()
